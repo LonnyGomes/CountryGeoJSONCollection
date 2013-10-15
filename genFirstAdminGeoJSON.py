c6f4d643bfce9a8e316ac2379beb58d8928a4889
Lonny Gomes <code@lonnygomes.com>
2013-10-15 10:36:51 -0400

    Cleaned up python script and added command line options

--- genFirstAdminGeoJSON.py
+++ genFirstAdminGeoJSON.py
@@ -2,22 +2,61 @@
 import requests
 import json
 import os
+import sys
+import getopt
+import string
 
-inputFile="countries.geojson"
 featureKey="ne_10m_adm"
+inputFile="countries.geojson"
 outputPath = "geojson"
+willForceUpdate=0
 #shouldLint = 1
-validate_endpoint = 'http://geojsonlint.com/validate'
+#validate_endpoint = 'http://geojsonlint.com/validate'
+
+#if no  parameters are supplied, go with defaults
+#otherwise parse the command line for the options
+if len(sys.argv) > 1:
+    #specify valid options
+    shortOpts = "k:i:of"
+    longOpts = [ "inputFile=", "outputPath=","keyName=","force"]
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], shortOpts, longOpts)
+        for opt,arg in opts:
+            if opt in ("-k", "--keyName"):
+                featureKey=arg
+            elif opt in ("-i", "--inputFile"):
+                inputFile=arg
+            elif opt in ("-o", "--outputPath"):
+                outputPath=arg
+            elif opt in ("-f", "--force"):
+                willForceUpdate = 1
+
+    except getopt.GetoptError as e:
+        print """Invalid syntax!
+        Usage: -k | --keyName <attribute key name in shape file> 
+               -i | --inputFile  <input geojson file> 
+               -o | --outputPath <output path>
+               -f if supplied, existing files will be overwritten
+        """
+        sys.exit(-1)
 
 try:
+    print "Reading geojson file: %s ..." % inputFile,
+    sys.stdout.flush()
+
     fp = open(inputFile)
     geoJSONList = fp.readlines()
     fp.close
+    print "DONE"
 except IOError:
     print "Could not open geojson file for reading:" + inputFile
     exit(-1)
 
+print "Parsing ...",
+sys.stdout.flush()
 geoJSONData = json.loads(' '.join(geoJSONList))
+print "DONE"
 
 geoFeatures = geoJSONData["features"]
 
@@ -29,9 +68,12 @@ for curFeature in geoFeatures:
 
     curFilename = outputPath + os.sep +  curKey + ".geojson"
 
-    if (os.path.exists(curFilename)):
-        print "File already exists!"
-        next
+    if os.path.exists(curFilename):
+        if willForceUpdate:
+            os.remove(curFilename)
+        else:
+            print "File already exists!"
+            next
     
     curGeoDataStr = json.dumps(curFeature)
     #lint_request = requests.post(validate_endpoint, data=curGeoDataStr)
